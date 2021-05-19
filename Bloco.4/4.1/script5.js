8ecf9ff72eeea031121c2cbbfee936110897a4a3
Sergio Espinoso Pires Junior <sergio.espp@gmail.com>
2021-05-19 20:23:13 -0300

    Programa 5 adicionado

---
+++ Bloco.4/4.1/script5.js
@@ -0,0 +1,17 @@
+let numUm = -60;
+let numDois = -60;
+let numTres = -60;
+
+let numZero = numUm > 0 && numDois > 0 && numTres > 0; 
+
+if (numZero) {
+  if (numUm + numDois + numTres == 180) {
+    console.log('True');
+  } 
+  else {
+    console.log('False');
+  }
+}
+else {
+  console.log('Numero negativo: Não é válido');
+}
