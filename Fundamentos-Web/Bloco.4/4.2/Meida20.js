1bfb2c0a49c60e2e32b71720ed1e0d00d5780f58
Sergio Espinoso Pires Junior <sergio.espp@gmail.com>
2021-05-20 19:04:51 -0300

    Exercicio media 2 adicionado

---
+++ Fundamentos-Web/Bloco.4/4.2/Meida20.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+
+for (index = 0; index < numbers.length; index += 1){
+  soma += numbers[index];
+  media = soma / numbers.length;
+ 
+}
+
+console.log('Soma é: ' + soma, ' Média é: ' + media);
+
+if (media > 20) {
+  console.log('Valor maior que 20');
+} 
+else {
+  console.log('Valor menor que 20');
+}
