b2eb1821602600972cc29cd97c9453067e96b465
Sergio Espinoso Pires Junior <sergio.espp@gmail.com>
2021-05-20 21:37:45 -0300

    Ultimos exercicios prontos

---
+++ Fundamentos-Web/Bloco.4/4.2/DivididoPorDois.js
@@ -0,0 +1,11 @@
+let vazio = [];
+
+for (index = 1; index < 26; index += 1){
+  vazio.push(index)  ; 
+}
+
+console.log(vazio);
+
+for (index = 0; index < vazio.length; index += 1) {
+  console.log(vazio[index] / 2);
+};
